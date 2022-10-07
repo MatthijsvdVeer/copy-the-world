dbeb18e8734470f2e60c163ef6783ca1599ef913
MatthijsvdVeer <MatthijsvdVeer@users.noreply.github.com>
2022-10-07 17:41:46 +0200

    Specified property name for component

--- src/CopyTheWorld/CopyTheWorld.Shared/TwinModels/Components/SpaceCO2.cs
+++ src/CopyTheWorld/CopyTheWorld.Shared/TwinModels/Components/SpaceCO2.cs
@@ -1,9 +1,11 @@
 ﻿namespace CopyTheWorld.Shared.TwinModels.Components;
 
 using Azure.DigitalTwins.Core;
+using System.Text.Json.Serialization;
 
 [Dtmi("dtmi:digitaltwins:rec_3_3:SpaceCO2;1")]
 public sealed class SpaceCo2 : BasicDigitalTwinComponent
 {
+    [JsonPropertyName("CO2Sensor")]
     public double CO2Sensor { get; set; }
 }
