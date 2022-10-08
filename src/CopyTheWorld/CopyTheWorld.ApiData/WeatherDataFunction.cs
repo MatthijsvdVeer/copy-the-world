c9a9c5c7167dd986f794454f754f05e56ee4682a
Matthijs van der Veer <subscriptions@vanderveer.io>
2022-10-08 12:43:11 +0200

    Excluding publish folder from deployment

--- src/CopyTheWorld/CopyTheWorld.ApiData/WeatherDataFunction.cs
+++ src/CopyTheWorld/CopyTheWorld.ApiData/WeatherDataFunction.cs
@@ -1,15 +1,14 @@
+namespace CopyTheWorld.ApiData;
+
 using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
-namespace CopyTheWorld.ApiData
+public class WeatherDataFunction
 {
-    public class WeatherDataFunction
+    [FunctionName("WeatherDataFunction")]
+    public void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
     {
-        [FunctionName("WeatherDataFunction")]
-        public void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
-        {
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-        }
+        log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
     }
 }
